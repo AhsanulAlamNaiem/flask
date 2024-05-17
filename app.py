398170694c3c79818166e72d6e86469d8612db2d
Md. Ahsanul Alam Naiem <ahsanulnaiem23@gmail.com>
2024-05-18 05:37:26 +0600

    a

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    return("<a href = 'https://www.google.com'>Hi</a>")
+    return(render_template("home.html"))
+
+@app.route("/<name>")
+def portfolio(name):
+    return(render_template("portfolio.html"))
+
+@app.route("/admin")
+
+def admin():
+    return(redirect(url_for("home")))  
