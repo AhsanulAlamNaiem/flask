76db9a01a9f27219151da810710ac4e25b7cb8aa
Md. Ahsanul Alam Naiem <ahsanulnaiem23@gmail.com>
2024-05-18 19:49:21 +0600

    added pptx file so that it can be downloaded. added an admin page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template,send_file
 
 app = Flask(__name__)
 
@@ -14,8 +14,12 @@ def portfolio(name):
 @app.route("/admin")
 
 def admin():
-    return(redirect(url_for("home")))  
+    return(render_template("admin.html"))  
 
+@app.route("/kaizen")
+def kaizen():
+    file="res/kaizen SUST.pptx"
+    return( send_file(file,as_attachment=True))
 
 if __name__ =="__main__":
     app.run(debug = True, port=8080, host = "0.0.0.0")
