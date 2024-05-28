b5cb1b60170f0fbbd9957a61fc047cf1d7514392
Md. Ahsanul Alam Naiem <ahsanulnaiem23@gmail.com>
2024-05-29 02:10:29 +0600

    app.py corrected again.

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def user():
         return(redirect(url_for("login")))
 
 
-# if __name__ =="__main__":
-#     app.run(port=8080, host = "0.0.0.0")
+if __name__ =="__main__":
+    app.run(port=8080, host = "0.0.0.0")
