4c8b8441dd259133b0a66c8d124a60754f201e03
Md. Ahsanul Alam Naiem <ahsanulnaiem23@gmail.com>
2024-06-02 21:31:12 +0600

    portfolio completed html broken css

--- app.py
+++ app.py
@@ -1,41 +1,25 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-import json
+import json, sqlite3
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(minutes=13)
 
+conn= sqlite3.connect("./res/portfoliodb.db")
+
 @app.route("/")
 def home():
-    return(render_template("home.html"))
-
-@app.route("/login",methods =["POST","GET"])
-def login():
-    if request.method =="POST":
-        session.permanent = True
-        user = request.form["username"]
-        session["user"] = user
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            return(redirect(url_for("user")))
-        else:
-            return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    session.pop("user",None)
-    return(redirect(url_for("login")))
-
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        flash("your have been loged out","info")
-        return(f"<h1>{user}</h>")
-    else:
-        return(redirect(url_for("login")))
+    db = sqlite3.connect('./res/portfoliodb.db')
+    skills = db.execute("Select skill,skill_type from skills").fetchall()
+    education = db.execute("Select Degree, startdate,  enddate, institute,description from education").fetchall()
+    certificates = db.execute("Select tittle, date, skill, description from certificates").fetchall()
+    services = db.execute("Select title, key from services").fetchall()
+    services_list =[]
+    for service in services:
+        skills = db.execute("SELECT GROUP_CONCAT(skill, ', ') AS concatenated_skills FROM skills WHERE {service} = 1;".format(service = service[1])).fetchone()[0]
+        services_list.append((service[0],skills))
+    return(render_template("home.html",services = services_list,education=education, certificates = certificates))
 
 
 if __name__ =="__main__":
